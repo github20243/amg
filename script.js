f0395c8fdc535d74a696ff69b2e82ed549658fed
Nurislam Abdimalikov <98372q@gmail.com>
2024-03-06 19:08:51 +0600

    gigant syima

---
+++ script.js
@@ -0,0 +1,22 @@
+const myArray = ['yellow',100, true, 'toyota']
+
+let [color, num] = myArray;
+
+console.log(color);
+
+console.log(num);
+
+const newObj = {
+    name: nurs,
+    addres: {
+    town: bishkek,
+    steet: Jalal-Abad
+    }
+}
+
+const {
+    name: nurs,
+    addres: {town, steet},
+} = newObj 
+
+console.log(town);
